5afb08a5b5e8b45d5151bb298975cf0b3694c20d
JustinLGates <papagates1@gmail.com>
2020-08-10 16:02:02 -0600

    Updates

---
+++ precache-manifest.ce3c53860c323105a73c420dc98769ed.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2febf301e71e7176a69918bc2eda5df",
+    "url": "/www.jusingatesportfolio.com/index.html"
+  },
+  {
+    "revision": "b6a70b80ee7398207151",
+    "url": "/www.jusingatesportfolio.com/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "97e63636bc8fb388b7b4",
+    "url": "/www.jusingatesportfolio.com/static/js/2.ca42dc3b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/www.jusingatesportfolio.com/static/js/2.ca42dc3b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b6a70b80ee7398207151",
+    "url": "/www.jusingatesportfolio.com/static/js/main.de2d5675.chunk.js"
+  },
+  {
+    "revision": "89047dfa2d925ee9e99f",
+    "url": "/www.jusingatesportfolio.com/static/js/runtime-main.8c210d5f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/www.jusingatesportfolio.com/static/media/logo.5d5d9eef.svg"
+  }
+]);
