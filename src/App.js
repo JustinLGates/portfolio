adfbe9c35dc388c7cdbb3388e13c771a2aa6db88
JustinLGates <papagates1@gmail.com>
2020-08-11 07:21:07 -0600

    added image test

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import me from "./me.jpg";
 
 function App() {
   return (
     <div className="App">
-      <h1>Software Developer Justin Gates</h1>
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col-12">
+            <h1>Software Developer Justin Gates</h1>
+          </div>
+        </div>
+
+        <div className="col-12 p-0 m-0 m-auto d-flex justify-content-center">
+          <div className="picContainer">
+            <img className="myPic" src={me} />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
